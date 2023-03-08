fe60bfe83d893b6e6ecceddf4c77c9c8b51b9157
ALEEF02 <thesmarty2002@gmail.com>
2023-03-08 09:15:25 -0500

    Add i2c fix

--- MPRLS/csvpressure.py
+++ MPRLS/csvpressure.py
@@ -1,14 +1,23 @@
+'''
+Made to log the time and pressure from the MPRLS in CSV format.
+Call with -u to leave the output unbuffered for tee
+
+python3.8 -u csvpressure.py |& tee output.csv
+'''
+
 import time
-import board
+from adafruit_extended_bus import ExtendedI2C as I2C
 import adafruit_mprls
 
-i2c = board.I2C()
+i2c = I2C(1)
+time.sleep(2) # Needed to ensure i2c is properly initialized
 
 # Simplest use, connect to default over I2C
 mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25)
 
+# Setup CSV headers
 print("Time(ms),Pressure(hPa)")
 
 while True:
-    print(str(round(time.time()*1000)*1000) + "," + str(mpr.pressure))
+    print(str(round(time.time()*1000)) + "," + str(mpr.pressure))
     time.sleep(60)
--- MPRLS/tankpressure-test.py
+++ MPRLS/tankpressure-test.py
@@ -1,8 +1,9 @@
 import time
-import board
+from adafruit_extended_bus import ExtendedI2C as I2C
 import adafruit_mprls
 
-i2c = board.I2C()
+i2c = I2C(1)
+time.sleep(2) # Needed to ensure i2c is properly initialized
 
 # Simplest use, connect to default over I2C
 mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25)
