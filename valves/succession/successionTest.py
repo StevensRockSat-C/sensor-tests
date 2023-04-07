b0ed5dee4a958a8b9410918e89a4b8129e492fd5
ALEEF02 <thesmarty2002@gmail.com>
2023-04-07 17:27:23 -0400

    Update succession test

--- valves/succession/successionTest.py
+++ valves/succession/successionTest.py
@@ -3,8 +3,13 @@ import time
 
 GPIO.setmode(GPIO.BOARD) # Use the board's physical pin numbers
 
-valve1 = 13
-valve2 = 15
+main_valve = 13
+
+bleed_valve = 15
+
+valve1 = 19
+valve2 = 21
+valve3 = 23
 
 """
 
@@ -18,9 +23,9 @@ valve2 = 15
   --> | GPIO27 (13) (14) GND                                  |
   --> | GPIO22 (15) (16) GPIO23                               |
       |  3V3  (17) (18) GPIO24                                |
-      | GPIO10 (19) (20) GND                                  |
-      |  GPIO9 (21) (22) GPIO25                               |
-      | GPIO11 (23) (24) GPIO8                                |
+  --> | GPIO10 (19) (20) GND                                  |
+  --> |  GPIO9 (21) (22) GPIO25                               |
+  --> | GPIO11 (23) (24) GPIO8                                |
       |    GND (25) (26) GPIO7                                |
       |  GPIO0 (27) (28) GPIO1                                |
       |  GPIO5 (29) (30) GND                                  |
@@ -34,28 +39,111 @@ valve2 = 15
 """
 
 # Set the pins to output
+GPIO.setup(main_valve, GPIO.OUT)
+GPIO.setup(bleed_valve, GPIO.OUT)
 GPIO.setup(valve1, GPIO.OUT)
 GPIO.setup(valve2, GPIO.OUT)
+GPIO.setup(valve3, GPIO.OUT)
 
-# Set both to low
-print("Both low")
+# Set all to low
+print("All low")
+GPIO.output(main_valve, GPIO.LOW)
+GPIO.output(bleed_valve, GPIO.LOW)
 GPIO.output(valve1, GPIO.LOW)
 GPIO.output(valve2, GPIO.LOW)
+GPIO.output(valve3, GPIO.LOW)
 
 time.sleep(1)
 
-print(valve1 + " high")
+print("valve1 high")
 GPIO.output(valve1, GPIO.HIGH)
 time.sleep(1)
-print(valve1 + " low")
+print("valve1 low\n")
 GPIO.output(valve1, GPIO.LOW)
 
 time.sleep(1)
 
-print(valve2 + " High")
+print("valve2 high")
 GPIO.output(valve2, GPIO.HIGH)
 time.sleep(1)
-print(valve2 + " low")
+print("valve2 low\n")
 GPIO.output(valve2, GPIO.LOW)
+
+time.sleep(1)
+
+print("valve3 high")
+GPIO.output(valve3, GPIO.HIGH)
+time.sleep(1)
+print("valve3 low\n")
+GPIO.output(valve3, GPIO.LOW)
+
+time.sleep(1)
+
+print("bleed_valve high")
+GPIO.output(bleed_valve, GPIO.HIGH)
+time.sleep(1)
+print("bleed_valve low\n")
+GPIO.output(bleed_valve, GPIO.LOW)
+
+time.sleep(1)
+
+print("main_valve high")
+GPIO.output(main_valve, GPIO.HIGH)
+time.sleep(1)
+print("main_valve low\n")
+GPIO.output(main_valve, GPIO.LOW)
+
+time.sleep(2)
+
+print("main_valve high")
+print("bleed_valve high")
+GPIO.output(main_valve, GPIO.HIGH)
+GPIO.output(bleed_valve, GPIO.HIGH)
+time.sleep(2)
+print("bleed_valve low")
+print("valve1 high")
+GPIO.output(bleed_valve, GPIO.LOW)
+GPIO.output(valve1, GPIO.HIGH)
+time.sleep(2)
+print("main_valve low")
+print("valve1 low\n")
+GPIO.output(main_valve, GPIO.LOW)
+GPIO.output(valve1, GPIO.LOW)
+
+time.sleep(2)
+
+print("main_valve high")
+print("bleed_valve high")
+GPIO.output(main_valve, GPIO.HIGH)
+GPIO.output(bleed_valve, GPIO.HIGH)
+time.sleep(2)
+print("bleed_valve low")
+print("valve2 high")
+GPIO.output(bleed_valve, GPIO.LOW)
+GPIO.output(valve2, GPIO.HIGH)
+time.sleep(2)
+print("main_valve low")
+print("valve2 low\n")
+GPIO.output(main_valve, GPIO.LOW)
+GPIO.output(valve2, GPIO.LOW)
+
+time.sleep(2)
+
+print("main_valve high")
+print("bleed_valve high")
+GPIO.output(main_valve, GPIO.HIGH)
+GPIO.output(bleed_valve, GPIO.HIGH)
+time.sleep(2)
+print("bleed_valve low")
+print("valve3 high")
+GPIO.output(bleed_valve, GPIO.LOW)
+GPIO.output(valve3, GPIO.HIGH)
+time.sleep(2)
+print("main_valve low")
+print("valve3 low")
+GPIO.output(main_valve, GPIO.LOW)
+GPIO.output(valve3, GPIO.LOW)
+
+time.sleep(1)
 
 GPIO.cleanup()
