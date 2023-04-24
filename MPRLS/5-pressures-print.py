c000d2647f21b126a79f81f0214419c510573fa1
ALEEF02 <thesmarty2002@gmail.com>
2023-04-24 16:19:53 -0400

    Clean up 5-pressure-print

--- MPRLS/5-pressures-print.py
+++ MPRLS/5-pressures-print.py
@@ -19,9 +19,7 @@ mpr0 = adafruit_mprls.MPRLS(multi[0], psi_min=0, psi_max=25)
 mpr1 = adafruit_mprls.MPRLS(multi[1], psi_min=0, psi_max=25)
 mpr2 = adafruit_mprls.MPRLS(multi[2], psi_min=0, psi_max=25)
 mpr3 = adafruit_mprls.MPRLS(multi[3], psi_min=0, psi_max=25)
-mpr4 = adafruit_mprls.MPRLS(multi[4], psi_min=0, psi_max=25)
-
-oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
+mpr4 = adafruit_mprls.MPRLS(multi[4], psi_min=0, psi_max=25)\
 
 # Setup CSV headers
 print("Time(ms),Pressure 0(hPa),Pressure 1(hpa),Pressure 2(hpa),Pressure 3(hpa),Pressure 4(hpa)")
