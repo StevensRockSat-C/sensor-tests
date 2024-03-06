3da9bb26dad32d22d101c35e9085246d63878c10
ALEEF02 <thesmarty2002@gmail.com>
2024-03-06 12:31:54 -0500

    using Multiprint in wrapper

--- Accelerometer/ADXL1001/daqHatWrapper.py
+++ Accelerometer/ADXL1001/daqHatWrapper.py
@@ -5,6 +5,8 @@ from daqhats_utils import select_hat_device, enum_mask_to_string, \
 from time import sleep
 from sys import stdout
 from multiprint import MultiPrinter
+
+
 class daqhatsWrapper:
     """
     Wrap the daqhat library for ease of use
@@ -20,7 +22,6 @@ class daqhatsWrapper:
 
         #FOR TESTING
         self.overrun = False
-        output_log = open(str(time.time()) + "_pressure.csv", 'x')
         
 
         #read from buffer, returns csv using printwriter
@@ -31,49 +32,40 @@ class daqhatsWrapper:
    
 
     def read_write_data(self, output_log):
-        total_samples_read = 0
         READ_ALL_AVAILABLE = -1
         samples_per_channel = 0
         read_request_size = READ_ALL_AVAILABLE
         self.hat.a_in_mode_write(AnalogInputMode.SE)
         self.hat.a_in_range_write(AnalogInputRange.BIP_10V) #change from 10v later?
         self.hat.a_in_scan_start(self.channelList, samples_per_channel, self.sample_rate, OptionFlags.CONTINUOUS)
-        csv_string = ''
+        mprint = MultiPrinter()
         timeout = 5.0
         input_mode = AnalogInputMode
+        
+        
+        def formatNum(n):
+            return '{:.5f}'.format(n)
+            
         while True:
             try:
                 read_result = self.hat.a_in_scan_read(read_request_size, timeout)
                 
-                #print("---- NEXT ONE -----")
-                #print(read_result.data)
-                
-                #stdout.flush()
-                
                 if (read_result.hardware_overrun | read_result.buffer_overrun):
                     self.overrun = True
-                samples_read_per_channel = int(len(read_result.data)/self.num_channels)
-
-                if samples_read_per_channel >0:
-                    index = samples_read_per_channel * self.num_channels - self.num_channels
                     
-                    for i in range (samples_read_per_channel):
-                        index = i * num_channels
-                        for j in range (num_channels):
-                            csv_string += '{:.5f},'.format(read_result.data[index+j])
-                    #for i in range(self.num_channels):
-                     #   csv_string += '{:.5f},'.format(read_result.data[index+i])
-                    stdout.flush()
+                mprint.p(",".join(map(lambda n: '{:.5f}'.format(n), read_result.data)), output_log) #joins all of data together with ,
+                #read_result.data clears every time you read it so you are getting the new data each time
+                stdout.flush()
                 sleep(0.1)
             except KeyboardInterrupt:
-                #print(csv_string)
-                print()
-                #print(','.join(
+                read_result = self.hat.a_in_scan_read(read_request_size, timeout)
+                mprint.p(",".join(map(lambda n: '{:.5f}'.format(n), read_result.data)), output_log) #joins all of data together with ,
                 exit()
         
             
     #        a_in_scan_cleanup()
 
+output_log = open("accelerations.csv", 'w')
 
-daq = daqhatsWrapper([1])
-daq.read_write_data()
+daq = daqhatsWrapper([1,2])
+daq.read_write_data(output_log)
