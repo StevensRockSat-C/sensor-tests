9171a961f73800d875d281903f795458c9b6215c
ALEEF02 <thesmarty2002@gmail.com>
2024-03-06 13:21:46 -0500

    Properly close files

--- Accelerometer/ADXL1001/daqHatWrapper.py
+++ Accelerometer/ADXL1001/daqHatWrapper.py
@@ -12,7 +12,7 @@ class daqhatsWrapper:
     Wrap the daqhat library for ease of use
     """
     #hat-id for 128 is 326, according to the library reference
-    def __init__ (self, chanList, hat_id=326, sample_rate=6400): #open port/channel
+    def __init__ (self, chanList=[1,2,3,4,5,6], hat_id=326, sample_rate=6400): #open port/channel
         self.hat_id = hat_id
         self.sample_rate = 6400 #with 6400 samples, can do 10 kS
         self.channelList = chan_list_to_mask(chanList)
@@ -23,18 +23,15 @@ class daqhatsWrapper:
         #FOR TESTING
         self.overrun = False
         
-
-        #read from buffer, returns csv using printwriter
         #put EVERYTHING in try catch for various errors
-        #
 
-        #close 
    
 
     def read_write_data(self, output_log):
         READ_ALL_AVAILABLE = -1
         samples_per_channel = 0
         read_request_size = READ_ALL_AVAILABLE
+        
         self.hat.a_in_mode_write(AnalogInputMode.SE)
         self.hat.a_in_range_write(AnalogInputRange.BIP_10V) #change from 10v later?
         self.hat.a_in_scan_start(self.channelList, samples_per_channel, self.sample_rate, OptionFlags.CONTINUOUS)
@@ -43,8 +40,10 @@ class daqhatsWrapper:
         input_mode = AnalogInputMode
         
         
-        def formatNum(n):
-            return '{:.5f}'.format(n)
+        actual_sampling_rate = self.hat.a_in_scan_actual_rate(self.num_channels, self.sample_rate)
+        #2nd arg = sample_rate_per_channel (float): The desired per-channel rate of the
+        #internal sampling clock, max 100,000.0.
+        
             
         while True:
             try:
@@ -53,19 +52,27 @@ class daqhatsWrapper:
                 if (read_result.hardware_overrun | read_result.buffer_overrun):
                     self.overrun = True
                     
-                mprint.p(",".join(map(lambda n: '{:.5f}'.format(n), read_result.data)), output_log) #joins all of data together with ,
+                mprint.p(",".join(map(lambda n: '{:.5f}'.format(n), read_result.data)), output_log) #joins all of 
+                #data together with , and appends to file
                 #read_result.data clears every time you read it so you are getting the new data each time
                 stdout.flush()
                 sleep(0.1)
             except KeyboardInterrupt:
-                read_result = self.hat.a_in_scan_read(read_request_size, timeout)
-                mprint.p(",".join(map(lambda n: '{:.5f}'.format(n), read_result.data)), output_log) #joins all of data together with ,
+                read_result = self.hat.a_in_scan_read(read_request_size, timeout) #get last values
+                mprint.p(",".join(map(lambda n: '{:.5f}'.format(n), read_result.data)), output_log) 
+                
+                #closing all files/scans
+                output_log.close()
+                self.hat.a_in_scan_stop() #stopping continuous scan
+                self.hat.a_in_scan_cleanup() #cleaning up
+                print("Overrun? ", self.overrun)
+                print("Actual sampling rate: ", actual_sampling_rate)
                 exit()
         
             
     #        a_in_scan_cleanup()
 
-output_log = open("accelerations.csv", 'w')
+output_log = open("accelerations.csv", 'w') #OVERWRITES OLD FILE
 
 daq = daqhatsWrapper([1,2])
 daq.read_write_data(output_log)
