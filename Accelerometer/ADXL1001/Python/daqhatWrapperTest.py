710b933942a11be9d714e9251db7bced0f801bc9
nlundquist1 <126936419+nlundquist1@users.noreply.github.com>
2024-03-27 11:46:35 -0400

    Add files via upload

---
+++ Accelerometer/ADXL1001/Python/daqhatWrapperTest.py
@@ -0,0 +1,12 @@
+import pytest
+import daqHatWrapper
+
+@pytest.fixture
+def wrapper():
+    return daqHatWrapper.daqhatsWrapper()
+
+
+
+def test_wrapper_init(wrapper):
+    assert wrapper.numChannels == 6
+    assert wrapper.sampleRate == 6400
